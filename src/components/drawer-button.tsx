3cffa0724abe851aacd4a9514f38c690a5d60155
Pietroks <131334815+Pietroks@users.noreply.github.com>
2024-04-06 18:36:56 -0300

    projeto finalizado

---
+++ src/components/drawer-button.tsx
@@ -0,0 +1,40 @@
+import { colors } from "@/styles/colors";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Pressable, PressableProps, Text, View } from "react-native";
+import clsx from "clsx";
+
+export type IconNameProps = keyof typeof MaterialIcons.glyphMap
+
+type DrawerButtonProps = PressableProps & {
+    title: string
+    isFocused?: boolean
+    isDivider?: boolean
+    iconName: IconNameProps
+    notification?: number
+}
+
+export function DrawerButton({title, isFocused, isDivider, iconName, notification, ...rest}: DrawerButtonProps) {
+    return (
+        <Pressable className={clsx('py-2 w-full',{
+            "border-b ml-10 border-gray-500": isDivider
+        })}
+        {...rest}>
+            <View className={clsx('flex-row items-center gap-4 h-14 px-6 -ml-2 w-full',{
+                "-ml-12": isDivider, "bg-orange-800 rounded-r-full": isFocused
+            })}>
+                <MaterialIcons name={iconName} size={20} color={isFocused ? colors.orange[300]: colors.gray[400]} />
+                
+                <Text className={clsx('text-white font-subtitle text-base flex-1', {
+                    "text-orange-300": isFocused
+                })}>
+                    {title}
+                </Text>
+
+                <Text className={clsx('text-gray-400 text-sm font-body', {
+                    "text-orange-300": isFocused
+                })}>{notification}</Text>
+            </View>
+            
+        </Pressable>
+    )
+}
---
+++ src/utils/emails.ts
@@ -0,0 +1,41 @@
+export type EmailDataProps = (typeof EMAILS)[0]
+
+export const EMAILS = [
+    {
+        id: '1',
+        name: 'Jebedaya Jackson',
+        avatar: 'https://mighty.tools/mockmind-api/content/robot/15.jpg',
+        marker: 'importante',
+        subject: 'What is Lorem Ipsum',
+        message: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+        start: false,
+        date: '13 de mar.'
+    },
+    {
+        id: '2',
+        name: 'Walter White',
+        avatar: 'https://mighty.tools/mockmind-api/content/robot/11.jpg',
+        subject: 'Where does it come from',
+        message: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+        start: true,
+        date: '14 de mar.'
+    },
+    {
+        id: '3',
+        name: 'Harmony Cobelt',
+        avatar: 'https://mighty.tools/mockmind-api/content/alien/3.jpg',
+        subject: 'Why do we use it',
+        message: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+        start: false,
+        date: '15 de mar.'
+    },
+    {
+        id: '4',
+        name: 'Harmony Cobelt',
+        avatar: 'https://mighty.tools/mockmind-api/content/alien/3.jpg',
+        subject: 'Why do we use it',
+        message: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+        start: false,
+        date: '15 de mar.'
+    }
+]
